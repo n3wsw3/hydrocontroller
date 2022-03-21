cbd2d7cb07aadee259e35ca999b57de674dc4ae6
Simon Uggla <simon.uggla@aol.com>
2022-03-21 21:37:37 +0100

    Added reaction to changing water and ec levels

--- Basic/Basic.ino
+++ Basic/Basic.ino
@@ -9,12 +9,14 @@ int pumps[] = {2, 3, 4, 5, 6};
 double water_level = 0;
 double temperature = 25;
 double ph = 0;
+double ec = 0;
 
 // This "ghetto" multi-threading solution works ONLY if the combined running time of all handler functions
 // is less than the lowest wait-time.
 Handler handlers[] = {{"Measure EC", &measure_EC, 40U}, {"Update Sensor Data", &update_sensor_data, 1000}, {"Send Data", &send_data, 30UL * 60 * 1000}};
 
 #define ecPin A1
+#define activatePumpPin 7
 
 void setup()
 {
@@ -30,19 +32,69 @@ void setup()
   }
 
   pinMode(ecPin, INPUT);
+  pinMode(activatePumpPin, INPUT);
 }
+
+double waterLevel_max = 25;
+double optimal_ec = 900;
+
+unsigned long switch_time = 0;
+int wait_time = 10000;
+int run_time = 1000;
+bool isWaiting = false;
 
 // the loop routine runs over and over again forever:
 void loop()
 {
-  for (auto& handler : handlers)
+  for (auto &handler : handlers)
   {
     if (handler.should_run())
     {
-      Log((String) "Running handler: " + handler.name);
+      // Log((String) "Running handler: " + handler.name);
       handler.run();
     }
   }
+
+  // There is too little water!
+  if (water_level < waterLevel_max)
+  {
+    set_pump(0, HIGH);
+  }
+  else
+  {
+    set_pump(0, LOW);
+  }
+
+  // If the measured ec is below the optimal
+  // Then switch between turning the pump on and
+  // off again. run_time is the time the pump is
+  // supposed to be on and wait_time is the
+  // time to wait between turning the pump on.
+  if (ec < optimal_ec)
+  {
+    if (!isWaiting)
+    {
+      set_pump(1, HIGH);
+    }
+    else
+    {
+      set_pump(1, LOW);
+    }
+
+    if (switch_time <= millis())
+    {
+      switch_time = millis() + (isWaiting ? run_time : wait_time);
+      isWaiting = !isWaiting;
+    }
+  } else {
+    set_pump(1, LOW);
+  }
+}
+
+void set_pump(unsigned int index, bool onoff)
+{
+  if (index < (sizeof(pumps) / sizeof(pumps[0])))
+    digitalWrite(pumps[index], onoff);
 }
 
 void update_sensor_data()
@@ -52,7 +104,7 @@ void update_sensor_data()
   // measure_EC();
   measure_pH();
 
-  Log((String) "wl: " + water_level + " t: " + temperature + " ec: " + get_EC() + " ph: " + ph);
+  Log((String) "wl: " + water_level + " t: " + temperature + " ec: " + ec + " ph: " + ph);
 }
 
 void send_data()
@@ -60,7 +112,7 @@ void send_data()
   StaticJsonDocument<64> doc;
   doc["waterLevel"] = water_level;
   doc["temperature"] = temperature;
-  doc["ec"] = get_EC();
+  doc["ec"] = ec;
   doc["ph"] = ph;
 
   serializeJson(doc, Serial);
@@ -74,7 +126,7 @@ void measure_waterLevel()
 
 void measure_temperature()
 {
-  // temperature = 4 * sin(((float)millis()) / 5000) + 20;
+  temperature = 4 * sin(((float)millis()) / 5000) + 20;
 }
 
 #define EC_BUFFER_COUNT 30
@@ -86,10 +138,7 @@ int ecBufferIndex = 0;
 void measure_EC()
 {
   ecbuffer[ecBufferIndex++ % EC_BUFFER_COUNT] = analogRead(ecPin);
-}
 
-float get_EC()
-{
   float averageVoltage = getMedianNum(ecbuffer, EC_BUFFER_COUNT) * VREF / 1024.0f;
   // Compensate for temperature differences.
   // Currently the temperature won't change since we don't measure temperature
@@ -99,8 +148,7 @@ float get_EC()
   float compensationVoltage = averageVoltage / compensationCoefficient;
   // tds ppm
   float tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
-  float ec = tdsValue * 1000.0f / 550.0f;
-  return ec;
+  ec = tdsValue * 1000.0f / 550.0f;
 }
 
 void measure_pH()
